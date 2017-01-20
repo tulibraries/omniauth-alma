f56c1b3603e933fc24375f00dfa32056dd6bd953
Chad Nelson <chadbnelson@gmail.com>
2017-01-20 16:15:44 -0500

    Dumb down for now

--- lib/omniauth/strategies/alma.rb
+++ lib/omniauth/strategies/alma.rb
@@ -1,58 +1,35 @@
-require 'alma'
+require 'omniauth'
 
 module OmniAuth
   module Strategies
     class Alma
       include OmniAuth::Strategy
+      include OmniAuth::Strategy
 
-      options :fields, [:username, :password]
-      options :uid_field, :username
-
-      option :on_failed_registration, nil
+      option :fields, [:username]
+      option :uid_field, :username
 
       uid do
         request.params[options.uid_field.to_s]
       end
 
       info do
-        {name: request.params[options.uid_field.to_s]}
-      end
-
-      def request_phase
-        if username && password
-          r = Rack::Response.new
-          r.redirect(callback_path, 307)
-          r.finish
-        else
-          OmniAuth::Form.build(
-              title: (options[:title] || "Alma Authentication"),
-              url: callback_path
-          ) do |f|
-            f.text_field 'Username', options.params.username
-            f.password_field 'Password', options.params.password
-          end.to_response
+        options.fields.inject({}) do |hash, field|
+          hash[field] = request.params[field.to_s]
+          hash
         end
       end
+      
 
-      def callback_phase
-        return fail!(:invalid_credentials) if not identity
-        super
+      def request_phase
+        form = OmniAuth::Form.new(:title => "Alma Username", :url => callback_path)
+        options.fields.each do |field|
+          form.text_field field.to_s.capitalize.gsub("_", " "), field.to_s
+        end
+        form.button "Sign In"
+        form.to_response
       end
 
-      def identity
-        return unless username && password
-        ::Alma::User.authenticate({user_id: username, password: password})
-      end
-
-      protected
-
-      def username
-        request.params[options.fields.username]
-      end
-
-      def password
-        request.params[options.fields.password]
-      end
     end
   end
 end
